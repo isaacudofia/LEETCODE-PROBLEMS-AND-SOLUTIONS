ad73a664c6ff04c001eb6067971f18fceb6faea9
isaacudofia <isaacudofia0@gmail.com>
2025-05-09 16:07:42 +0100

    twoSum fixed properly

--- twoSum.js
+++ twoSum.js
@@ -5,12 +5,14 @@
 //Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 const nums = [2, 7, 11, 15];
-target = 9;
+target = 26;
 
 const twoSum = (nums, target) => {
   for (let i = 0; i <= nums.length - 1; i++) {
-    if (nums[i] + nums[i + 1] === target) {
-      return console.log([i, i + 1]);
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      if (nums[i] + nums[j] === target) {
+        return console.log([i, j]);
+      }
     }
   }
 };
