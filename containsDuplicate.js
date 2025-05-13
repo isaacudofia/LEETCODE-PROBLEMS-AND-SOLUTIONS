2963045f62ddce366dfcc48dc518aed6975f8bba
isaacudofia <isaacudofia0@gmail.com>
2025-05-13 21:39:14 +0100

    commit fixed solution

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -16,18 +16,21 @@
 
 // Output: false
 
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
 const nums = [1, 2, 3, 1];
 
 const containsDuplicate = (nums) => {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      console.log(`i: ${nums[i]} and j: ${nums[j]}`);
-      if (nums[i] === nums[j]) {
-        return true;
-      }
+  const seen = new Set(); // Create a Set to store unique elements
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return true; // Return true if a duplicate is found
     }
+    seen.add(num); // Add the current number to the Set
   }
-  return false;
+
+  return false; // Return false if no duplicates are found
 };
 
 console.log(containsDuplicate(nums));
